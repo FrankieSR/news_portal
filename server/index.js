c85759c59c0178deba1813f9d554e1e68f0c28c9
a.doroshko <a.doroshko@aws4.net>
2018-05-14 18:58:02 +0300

    comment: added files from homework

---
+++ server/index.js
@@ -0,0 +1,5 @@
+const parsePost = require('./parsePost');
+const sonfigs = require('./configs.js');
+
+
+parsePost('http://football.ua/ukraine/362045-upl-sbornaja-31-go-tura.html', elems.elems.footballua);
